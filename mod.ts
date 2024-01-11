786a13ee281990fd2d8d29a2c5d60cdcbb70598f
Oscar Otero <oom@oscarotero.com>
2024-01-11 21:35:26 +0100

    first commit

---
+++ mod.ts
@@ -0,0 +1,48 @@
+import cms from "cms/mod.ts";
+import { Octokit } from "npm:octokit";
+import { GhDataStorage, GhFileStorage } from "cms/src/storage/github.ts";
+
+const app = cms();
+const octokit = new Octokit({ auth: Deno.env.get("GITHUB_TOKEN") });
+
+// Create a new storage for the data
+app.data(
+  "fs",
+  new GhDataStorage({
+    client: octokit,
+    owner: "oscarotero",
+    repo: "test",
+  }),
+);
+
+// Create a new storage for the uploads
+app.files(
+  "uploads",
+  new GhFileStorage({
+    client: octokit,
+    owner: "oscarotero",
+    repo: "test",
+    path: "uploads",
+  }),
+);
+
+app.collection(
+  "posts",
+  "fs",
+  [
+    "title: text",
+    {
+      name: "summary",
+      type: "textarea",
+      attributes: {
+        required: true,
+      },
+    },
+    "tags: list",
+    "draft: checkbox",
+    "show_toc: checkbox",
+    "content: markdown",
+  ],
+);
+
+app.serve();
